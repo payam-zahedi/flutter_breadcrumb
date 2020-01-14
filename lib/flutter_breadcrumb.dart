bf0fd03f97c7a676e051e326f3c97f7c46de6e77
payam <pzahedi1995@yahoo.com>
2020-01-14 20:02:52 +0330

    initial commit

---
+++ lib/flutter_breadcrumb.dart
@@ -0,0 +1,14 @@
+library flutter_breadcrumb;
+
+import 'package:flutter/material.dart';
+
+class BreadCrumb extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class BreadCrumbItem {
+
+}
