fd0f354b121f54c812495918e3ff3e972a0c9cc3
payam <pzahedi1995@yahoo.com>
2020-01-15 23:35:25 +0330

    WrapBreadcrumb created

--- lib/src/breadcrumb.dart
+++ lib/src/breadcrumb.dart
@@ -33,8 +33,56 @@ class BreadCrumb extends StatelessWidget {
         overflowType: overflowType,
       );
 
+  Widget _buildFactory(BreadCrumbOverflow overflowType) {
+    switch (overflowType) {
+      case BreadCrumbOverflow.wrap:
+        return _WrapBreadCrumb(items: items,divider: divider);
+        break;
+      case BreadCrumbOverflow.ellipsisStart:
+        // TODO: Handle this case.
+        break;
+      case BreadCrumbOverflow.ellipsisEnd:
+        // TODO: Handle this case.
+        break;
+      case BreadCrumbOverflow.scroll:
+        // TODO: Handle this case.
+        break;
+      default:
+        return _WrapBreadCrumb(items: items,divider: divider);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return _buildFactory(overflowType);
+  }
+}
+
+class _WrapBreadCrumb extends StatelessWidget {
+  final List<BreadCrumbItem> items;
+  final Widget divider;
+
+  const _WrapBreadCrumb({Key key, this.items, this.divider})
+      : assert(items != null, 'items parameters should not be empty'),
+        assert(items.length > 0, 'items parameters should not be empty'),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> widgetItems;
+
+    if (divider != null) {
+      widgetItems=[];
+      items.forEach((item) {
+        widgetItems.add(item.content);
+        widgetItems.add(divider);
+      });
+    }
+    widgetItems ??= items.map<Widget>((item) => item.content);
+
+    return Wrap(
+      children: widgetItems,
+      crossAxisAlignment: WrapCrossAlignment.center,
+    );
   }
 }
