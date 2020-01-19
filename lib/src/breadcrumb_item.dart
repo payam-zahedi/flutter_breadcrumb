10ab752a47a659eb7572bf229c6b13da03c38b2c
payam <pzahedi1995@yahoo.com>
2020-01-19 21:52:59 +0330

    BreadCrumbItem updated and now have more fields

--- lib/src/breadcrumb_item.dart
+++ lib/src/breadcrumb_item.dart
@@ -1,25 +1,36 @@
-
 import 'package:flutter/material.dart';
 
 class BreadCrumbItem {
   final Widget content;
   final EdgeInsetsGeometry padding;
+  final EdgeInsetsGeometry margin;
   final VoidCallback onTap;
-  final Color enableColor;
+  final Color color;
   final Color disableColor;
+  final Color splashColor;
+  final Color textColor;
+  final Color disabledTextColor;
+  final BorderRadiusGeometry borderRadius;
+  final BorderSide border;
 
   BreadCrumbItem({
     @required this.content,
     this.padding,
+    this.margin,
     this.onTap,
-    this.enableColor = Colors.transparent,
+    this.color = Colors.transparent,
     this.disableColor = Colors.transparent,
+    this.splashColor = Colors.transparent,
+    this.textColor,
+    this.disabledTextColor,
+    this.borderRadius = BorderRadius.zero,
+    this.border = BorderSide.none,
   }) : assert(content != null);
 
   bool get isEnable => onTap != null;
 
   @override
   String toString() {
-    return 'BreadCrumbItem{content: $content, padding: $padding, onTap: $onTap, enableColor: $enableColor, disableColor: $disableColor}';
+    return 'BreadCrumbItem{content: $content, padding: $padding, margin: $margin, onTap: $onTap, enableColor: $color, disableColor: $disableColor, splashColor: $splashColor, textColor: $textColor, disabledTextColor: $disabledTextColor, borderRadiusGeometry: $borderRadius, border: $border}';
   }
 }
