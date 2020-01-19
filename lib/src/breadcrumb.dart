656637d208740a61aa298c5d5e05a451b51948f6
payam <pzahedi1995@yahoo.com>
2020-01-19 22:11:57 +0330

    BreadCrumbOverflow refactored to abstract class

--- lib/src/breadcrumb.dart
+++ lib/src/breadcrumb.dart
@@ -12,7 +12,7 @@ class BreadCrumb extends StatelessWidget {
     Key key,
     @required this.items,
     this.divider,
-    this.overflowType = BreadCrumbOverflow.wrap,
+    this.overflowType =const WrapOverflow(),
   })  : assert(items != null, 'items parameters should not be empty'),
         assert(items.length > 0, 'items parameters should not be empty'),
         assert(overflowType != null, 'overflowType parameter required'),
@@ -22,7 +22,7 @@ class BreadCrumb extends StatelessWidget {
     @required int itemCount,
     @required IndexedBreadCrumbItemBuilder builder,
     Widget divider,
-    BreadCrumbOverflow overflowType = BreadCrumbOverflow.wrap,
+    BreadCrumbOverflow overflowType =const WrapOverflow(),
   }) =>
       BreadCrumb(
         items: List<BreadCrumbItem>.generate(
@@ -33,70 +33,18 @@ class BreadCrumb extends StatelessWidget {
         overflowType: overflowType,
       );
 
-  Widget _buildFactory(BreadCrumbOverflow overflowType) {
-    switch (overflowType) {
-      case BreadCrumbOverflow.wrap:
-        return _WrapBreadCrumb(items: items, divider: divider);
-        break;
-      case BreadCrumbOverflow.ellipsisStart:
-        // TODO: Handle this case.
-        break;
-      case BreadCrumbOverflow.ellipsisEnd:
-        // TODO: Handle this case.
-        break;
-      case BreadCrumbOverflow.scroll:
-        // TODO: Handle this case.
-        break;
-      default:
-        return _WrapBreadCrumb(items: items, divider: divider);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
-    return _buildFactory(overflowType);
+    return overflowType.build(context,items,divider);
   }
 }
 
-class _WrapBreadCrumb extends StatelessWidget {
-  final List<BreadCrumbItem> items;
-  final Widget divider;
 
-  const _WrapBreadCrumb({Key key, this.items, this.divider})
-      : assert(items != null, 'items parameters should not be empty'),
-        assert(items.length > 0, 'items parameters should not be empty'),
-        super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    List<Widget> widgetItems;
-
-    if (divider != null) {
-      widgetItems = [];
-      items.forEach((item) {
-        widgetItems.add(
-          _BreadCrumbTile(breadCrumbItem: item),
-        );
-        widgetItems.add(divider);
-      });
-    }
-
-    widgetItems ??= items.map<Widget>(
-      (item) => _BreadCrumbTile(breadCrumbItem: item),
-    );
-
-    return Wrap(
-      children: widgetItems,
-      crossAxisAlignment: WrapCrossAlignment.center,
-      runSpacing: 4,
-    );
-  }
-}
-
-class _BreadCrumbTile extends StatelessWidget {
+class BreadCrumbTile extends StatelessWidget {
   final BreadCrumbItem breadCrumbItem;
 
-  const _BreadCrumbTile({Key key, @required this.breadCrumbItem})
+  const BreadCrumbTile({Key key, @required this.breadCrumbItem})
       : assert(breadCrumbItem != null),
         super(key: key);
 
--- lib/src/breadcrumb_overflow.dart
+++ lib/src/breadcrumb_overflow.dart
@@ -1,6 +1,51 @@
-enum BreadCrumbOverflow{
-  wrap,
-  ellipsisStart,
-  ellipsisEnd,
-  scroll,
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_breadcrumb/flutter_breadcrumb.dart';
+
+abstract class BreadCrumbOverflow {
+  const BreadCrumbOverflow();
+
+  Widget build(
+    BuildContext context,
+    List<BreadCrumbItem> items,
+    Widget divider,
+  );
+}
+
+class WrapOverflow extends BreadCrumbOverflow {
+  final double spacing;
+  final double runSpacing;
+
+  const WrapOverflow({
+    this.spacing = 0,
+    this.runSpacing = 0,
+  });
+
+  @override
+  Widget build(
+    BuildContext context,
+    List<BreadCrumbItem> items,
+    Widget divider,
+  ) {
+    List<Widget> widgetItems;
+
+    if (divider != null) {
+      widgetItems = [];
+      items.forEach((item) {
+        widgetItems.add(
+          BreadCrumbTile(breadCrumbItem: item),
+        );
+        widgetItems.add(divider);
+      });
+    }
+
+    widgetItems ??= items.map<Widget>(
+      (item) => BreadCrumbTile(breadCrumbItem: item),
+    );
+
+    return Wrap(
+      children: widgetItems,
+      crossAxisAlignment: WrapCrossAlignment.center,
+      runSpacing: 4,
+    );
+  }
 }
