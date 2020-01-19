1cf97def67f4e2e432140be9b5c154289bb2a6bd
payam <pzahedi1995@yahoo.com>
2020-01-19 22:53:16 +0330

    document added to breadcrumb_overflow file

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,32 +24,34 @@ class ExampleApp extends StatelessWidget {
         title: Text('Flutter Breadcrump'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            BreadCrumb.builder(
-              itemCount: 5,
-              builder: (index) {
-                return BreadCrumbItem(
-                  content: Text('text'),
-                  color: Colors.red,
-                  padding: EdgeInsets.all(8),
-                  borderRadius: BorderRadius.circular(4),
-                  disabledTextColor: Colors.green,
-                  textColor: Colors.yellow,
-                  border: BorderSide(color: Colors.blue),
-                  disableColor: Colors.deepPurple,
-                  splashColor: Colors.blue,
-                  onTap: index % 3 == 0
-                      ? () {
-                          print('item Clicked');
-                        }
-                      : null,
-                );
-              },
-              divider: Icon(Icons.chevron_right),
-            ),
-          ],
+        child: BreadCrumb.builder(
+          itemCount: 12,
+          builder: (index) {
+            return BreadCrumbItem(
+              content: Text('text'),
+              color: Colors.red,
+              padding: EdgeInsets.all(8),
+              borderRadius: BorderRadius.circular(4),
+              disabledTextColor: Colors.green,
+              textColor: Colors.yellow,
+              border: BorderSide(color: Colors.blue),
+              disableColor: Colors.deepPurple,
+              splashColor: Colors.blue,
+              onTap: index % 3 == 0
+                  ? () {
+                      print('item Clicked');
+                    }
+                  : null,
+            );
+          },
+          divider: Icon(Icons.chevron_right),
+          overflowType: WrapOverflow(
+            spacing: 8,
+            runSpacing: 8,
+            alignment: WrapAlignment.center,
+            runAlignment: WrapAlignment.start,
+            direction: Axis.vertical
+          ),
         ),
       ),
     );
--- lib/src/breadcrumb_overflow.dart
+++ lib/src/breadcrumb_overflow.dart
@@ -4,8 +4,11 @@ import 'package:flutter_breadcrumb/flutter_breadcrumb.dart';
 abstract class BreadCrumbOverflow {
   const BreadCrumbOverflow();
 
+  /// creates List<Widget> with [items] and [divider] parameters
+  /// and this list will use in [build] method
   List<Widget> widgetItems(List<BreadCrumbItem> items, Widget divider);
 
+  ///creates widget that should show [items] and [divider]
   Widget build(
     BuildContext context,
     List<BreadCrumbItem> items,
@@ -14,27 +17,42 @@ abstract class BreadCrumbOverflow {
 }
 
 class WrapOverflow extends BreadCrumbOverflow {
-  final WrapAlignment alignment;
-  final WrapAlignment runAlignment;
-  final WrapCrossAlignment crossAxisAlignment;
-  final Axis direction;
-  final double spacing;
-  final double runSpacing;
-  final TextDirection textDirection;
-  final VerticalDirection verticalDirection;
-
   const WrapOverflow({
     this.direction = Axis.horizontal,
     this.alignment = WrapAlignment.start,
     this.spacing = 0.0,
     this.runAlignment = WrapAlignment.start,
     this.runSpacing = 0.0,
-    this.crossAxisAlignment = WrapCrossAlignment.start,
+    this.crossAxisAlignment = WrapCrossAlignment.center,
     this.textDirection,
     this.verticalDirection = VerticalDirection.down,
   });
 
+  /// align children in main direction
+  final WrapAlignment alignment;
 
+  /// How the runs themselves should be placed in the cross axis.
+  final WrapAlignment runAlignment;
+
+  /// align children in cross direction
+  final WrapCrossAlignment crossAxisAlignment;
+
+  /// The direction to use as the main axis.
+  /// [Axis.horizontal] to show children in horizontal axis
+  /// and [Axis.vertical] is for show children in vertical axis
+  final Axis direction;
+
+  ///creates space between items in main axis
+  final double spacing;
+
+  ///creates space between items in cross axis
+  final double runSpacing;
+
+  ///it can be use in horizontal direction to show items in reverse
+  final TextDirection textDirection;
+
+  ///it can be use in vertical direction to show items in reverse
+  final VerticalDirection verticalDirection;
 
   @override
   List<Widget> widgetItems(List<BreadCrumbItem> items, Widget divider) {
